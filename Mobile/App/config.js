a5b10e0b74570bec3fefdc941659ed59e129f407
JUNG0821 <bak3353@naver.com>
2024-06-13 19:12:34 +0900

    잘안가져와졌는데?

--- Mobile/App/config.js
+++ Mobile/App/config.js
@@ -1,6 +1,6 @@
 const config = {
-    serverUrl: 'http://172.16.117.92:3000',
-    photoUrl : 'http://172.16.117.92:3000'
+    serverUrl: 'http://172.29.8.166:3000',
+    photoUrl : 'http://172.29.8.166:3000'
 };
 
 export default config;
