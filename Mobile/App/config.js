4973be6e72b5d92b3e1b0113ce651c33e5bc5d99
JUNG0821 <bak3353@naver.com>
2024-06-15 15:45:55 +0900

    pull 받아오자

--- Mobile/App/config.js
+++ Mobile/App/config.js
@@ -1,6 +1,6 @@
 const config = {
-    serverUrl: 'http://172.16.117.170:3000',
-    photoUrl : 'http://172.16.117.170:3000'
+    serverUrl: 'http://172.29.8.166:3000',
+    photoUrl : 'http://172.29.8.166:3000'
 };
 
 export default config;
