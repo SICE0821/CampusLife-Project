08d71d0654d66668c2f2ba3b8765adef3db7dcf4
epuchi <jite1214@gmail.com>
2024-06-16 04:38:26 +0900

    유저 관리 수정

--- Mobile/App/admin_screen/ManagementUserScreen.tsx
+++ Mobile/App/admin_screen/ManagementUserScreen.tsx
@@ -7,25 +7,19 @@ import IconSetting from 'react-native-vector-icons/Entypo';
 const width = Dimensions.get("window").width;
 
 const admin_check = ['관리자', '일반유저', '학과대표', '학과조교'];
-const departments = ['컴퓨터소프트웨어학과', '기계공학과', '전자공학과']; // Add more departments as needed
+const departments = ['컴퓨터소프트웨어학과', '기계공학과', '전자공학과']; // 필요에 따라 학과를 추가하세요
 
 const userData = [
-  { image: require('../assets/event1.jpg'), name: '홍길동', id: 'Hong01', department: '컴퓨터소프트웨어학과', report: 1, admin: 1, point: 2000 },
-  { image: require('../assets/event1.jpg'), name: '김철수', id: 'Kim01', department: '기계공학과', report: 4, admin: 0, point: 1500 },
-  { image: require('../assets/event1.jpg'), name: '이영희', id: 'Lee01', department: '전자공학과', report: 2, admin: 2, point: 1800 },
-  { image: require('../assets/event1.jpg'), name: '박지민', id: 'Park01', department: '컴퓨터소프트웨어학과', report: 3, admin: 3, point: 2200 },
-  { image: require('../assets/event1.jpg'), name: '홍길동', id: 'Hong01', department: '컴퓨터소프트웨어학과', report: 1, admin: 1, point: 2000 },
-  { image: require('../assets/event1.jpg'), name: '김철수', id: 'Kim01', department: '기계공학과', report: 4, admin: 0, point: 1500 },
-  { image: require('../assets/event1.jpg'), name: '이영희', id: 'Lee01', department: '전자공학과', report: 2, admin: 2, point: 1800 },
-  { image: require('../assets/event1.jpg'), name: '박지민', id: 'Park01', department: '컴퓨터소프트웨어학과', report: 3, admin: 3, point: 2200 },
-  { image: require('../assets/event1.jpg'), name: '홍길동', id: 'Hong01', department: '컴퓨터소프트웨어학과', report: 1, admin: 1, point: 2000 },
-  { image: require('../assets/event1.jpg'), name: '김철수', id: 'Kim01', department: '기계공학과', report: 4, admin: 0, point: 1500 },
-  { image: require('../assets/event1.jpg'), name: '이영희', id: 'Lee01', department: '전자공학과', report: 2, admin: 2, point: 1800 },
-  { image: require('../assets/event1.jpg'), name: '박지민', id: 'Park01', department: '컴퓨터소프트웨어학과', report: 3, admin: 3, point: 2200 },
-  { image: require('../assets/event1.jpg'), name: '홍길동', id: 'Hong01', department: '컴퓨터소프트웨어학과', report: 1, admin: 1, point: 2000 },
-  { image: require('../assets/event1.jpg'), name: '김철수', id: 'Kim01', department: '기계공학과', report: 4, admin: 0, point: 1500 },
-  { image: require('../assets/event1.jpg'), name: '이영희', id: 'Lee01', department: '전자공학과', report: 2, admin: 2, point: 1800 },
-  { image: require('../assets/event1.jpg'), name: '박지민', id: 'Park01', department: '컴퓨터소프트웨어학과', report: 3, admin: 3, point: 2200 },
+  { userId: 1, image: require('../assets/event1.jpg'), name: '홍길동', id: 'Hong01', department: '컴퓨터소프트웨어학과', report: 1, admin: 1, point: 2000 },
+  { userId: 2, image: require('../assets/event1.jpg'), name: '김철수', id: 'Kim01', department: '기계공학과', report: 4, admin: 0, point: 1500 },
+  { userId: 3, image: require('../assets/event1.jpg'), name: '이영희', id: 'Lee01', department: '전자공학과', report: 2, admin: 2, point: 1800 },
+  { userId: 4, image: require('../assets/event1.jpg'), name: '홍길동', id: 'Hong01', department: '컴퓨터소프트웨어학과', report: 1, admin: 1, point: 2000 },
+  { userId: 5, image: require('../assets/event1.jpg'), name: '김철수', id: 'Kim01', department: '기계공학과', report: 4, admin: 0, point: 1500 },
+  { userId: 6, image: require('../assets/event1.jpg'), name: '이영희', id: 'Lee01', department: '전자공학과', report: 2, admin: 2, point: 1800 },
+  { userId: 7, image: require('../assets/event1.jpg'), name: '홍길동', id: 'Hong01', department: '컴퓨터소프트웨어학과', report: 1, admin: 1, point: 2000 },
+  { userId: 8, image: require('../assets/event1.jpg'), name: '김철수', id: 'Kim01', department: '기계공학과', report: 4, admin: 0, point: 1500 },
+  { userId: 9, image: require('../assets/event1.jpg'), name: '이영희', id: 'Lee01', department: '전자공학과', report: 2, admin: 2, point: 1800 },
+
 ];
 
 const getRoleColor = (role: number) => {
@@ -46,7 +40,9 @@ const getRoleColor = (role: number) => {
 const ManagementUserScreen = () => {
   const [selectedDepartment, setSelectedDepartment] = useState('');
   const [selectedAdmin, setSelectedAdmin] = useState('');
+  const [selectedUser, setSelectedUser] = useState<any>(null); // 선택된 사용자 상태
 
+  // 사용자 데이터를 필터링하여 선택된 학과와 관리자 유형에 따라 필터링합니다.
   const filteredUserData = userData.filter((user) => {
     return (
       (selectedDepartment === '' || user.department === selectedDepartment) &&
@@ -54,8 +50,38 @@ const ManagementUserScreen = () => {
     );
   });
 
+  // 각 사용자의 설정 아이콘을 클릭하여 선택된 사용자 상태를 업데이트합니다.
+  const setSettingUser = (user: any) => {
+    setSelectedUser(user);
+  };
+
+  // 신고 확인 기능
+  const handleReportCheck = () => {
+    if (selectedUser) {
+      console.log(`선택된 유저의 신고 누적 횟수: ${selectedUser.report}`);
+      // 여기에 신고 확인 기능을 구현할 수 있습니다.
+    }
+  };
+
+  // 권한 변경 기능
+  const handleChangeRole = () => {
+    if (selectedUser) {
+      console.log(`선택된 유저의 현재 권한: ${admin_check[selectedUser.admin]}`);
+      // 여기에 권한 변경 기능을 구현할 수 있습니다.
+    }
+  };
+
+  // 포인트 수정 기능
+  const handleModifyPoints = () => {
+    if (selectedUser) {
+      console.log(`선택된 유저의 현재 포인트: ${selectedUser.point}`);
+      // 여기에 포인트 수정 기능을 구현할 수 있습니다.
+    }
+  };
+
   return (
     <View style={styles.container}>
+      {/* 학과 및 관리자 유형 선택 피커 */}
       <View style={styles.pickerContainer}>
         <Picker
           selectedValue={selectedDepartment}
@@ -78,6 +104,8 @@ const ManagementUserScreen = () => {
           ))}
         </Picker>
       </View>
+
+      {/* 사용자 목록 스크롤 뷰 */}
       <ScrollView>
         {filteredUserData.map((user, index) => (
           <View key={index} style={styles.userInfoBox}>
@@ -101,13 +129,32 @@ const ManagementUserScreen = () => {
               </View>
             </View>
             <View style={styles.userSettingArea}>
-              <TouchableOpacity style={styles.userSettingBtn}>
+              {/* 설정 아이콘 버튼 */}
+              <TouchableOpacity
+                style={styles.userSettingBtn}
+                onPress={() => setSettingUser(user)}
+              >
                 <IconSetting style={styles.userSettingIcon} name='dots-three-vertical' />
               </TouchableOpacity>
+
+              
             </View>
+            {/* 선택된 사용자에 대한 드롭다운 메뉴 */}
+            {selectedUser && selectedUser.userId === user.userId && (
+                <View style={styles.dropdown}>
+                  <TouchableOpacity style={styles.dropdownItem} onPress={handleReportCheck}>
+                    <Text style={styles.dropdownText}>신고확인</Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity style={styles.dropdownItem} onPress={handleChangeRole}>
+                    <Text style={styles.dropdownText}>권한 변경</Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity style={styles.dropdownItem} onPress={handleModifyPoints}>
+                    <Text style={styles.dropdownText}>포인트 수정</Text>
+                  </TouchableOpacity>
+                </View>
+              )}
           </View>
         ))}
-        <View style={{ height: 100 }}></View>
       </ScrollView>
     </View>
   );
@@ -127,6 +174,8 @@ const styles = StyleSheet.create({
   picker: {
     height: 50,
     width: width * 0.45,
+    backgroundColor: 'white',
+    elevation: 3,
   },
   userInfoBox: {
     backgroundColor: 'white',
@@ -189,17 +238,34 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
   },
   userSettingArea: {
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
     alignItems: 'center',
     width: 40,
   },
   userSettingBtn: {
-    padding: 5,
+    paddingVertical: 10,
   },
   userSettingIcon: {
     color: 'black',
     fontSize: 24,
   },
+  dropdown: {
+    width: 110,
+    position: 'absolute',
+    right: 10,
+    top: 10,
+    backgroundColor: 'white',
+    elevation: 5,
+    borderRadius: 10,
+    paddingHorizontal: 10,
+  },
+  dropdownItem: {
+    paddingVertical: 5,
+  },
+  dropdownText: {
+    color: 'black',
+    fontSize: 18
+  }
 });
 
 export default ManagementUserScreen;
