e6cfddbcf4b79ce03aa68f6fe349ebcfa528e159
epuchi <jite1214@gmail.com>
2024-09-24 01:52:14 +0900

    Merge branch 'Feat/UI' into dev
    # Conflicts:
    #	Mobile/App/config.js

--- Web/src/page/config.js
+++ Web/src/page/config.js
@@ -1,6 +1,6 @@
 const config = {
-    serverUrl: 'http://192.168.35.135:4000',
-    photoUrl : 'http://192.168.35.135:4000'
+    serverUrl: 'http://14.6.152.120:4000',
+    photoUrl : 'http://14.6.152.120:4000'
 };
 
 export default config;
