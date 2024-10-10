b1772143bb24e1cce8d555f5dc986690d6a3f07d
epuchi <jite1214@gmail.com>
2024-10-10 19:06:58 +0900

    뱃지 추가

--- Mobile/App/screens/CardScreens/AcademicScreens/AcademicInfoScreen.tsx
+++ Mobile/App/screens/CardScreens/AcademicScreens/AcademicInfoScreen.tsx
@@ -398,70 +398,6 @@ const AcademicInfoScreen = ({ route }: any) => {
     return (
         <View style={styles.container}>
             <ScrollView>
-                {/* 뱃지 영역 */}
-                <View style={styles.badgeTitle}>
-                    <Text style={styles.badgeTitleText}>내 뱃지</Text>
-                    <IconB style={styles.badgeIcon} name="badge" size={30} />
-                </View>
-                <ScrollView horizontal={true} style={styles.badgeContainer}>
-                    {/* 개별 뱃지 일단 반투명 하게 하고 얻으면 불투명하게 하면 될듯?*/}
-                    <View style={styles.badgeItem}>
-                        <Image
-                            source={require('../../../assets/뱃지.png')}
-                            resizeMode="contain"
-                            style={styles.badgeImage}
-                        />
-                        <Text style={styles.badgeText}>우등상</Text>
-                    </View>
-                    <View style={styles.badgeItem}>
-                        <Image
-                            source={require('../../../assets/뱃지2.png')}
-                            resizeMode="contain"
-                            style={styles.badgeImage}
-                        />
-                        <Text style={styles.badgeText}>수강상</Text>
-                    </View>
-                    <View style={styles.badgeItem}>
-                        <Image
-                            source={require('../../../assets/뱃지3.png')}
-                            resizeMode="contain"
-                            style={styles.badgeImage}
-                        />
-                        <Text style={styles.badgeText}>성장상</Text>
-                    </View>
-                    <View style={styles.badgeItem}>
-                        <Image
-                            source={require('../../../assets/뱃지4.png')}
-                            resizeMode="contain"
-                            style={styles.badgeImage}
-                        />
-                        <Text style={styles.badgeText}>노력상</Text>
-                    </View>
-                    <View style={styles.badgeItem}>
-                        <Image
-                            source={require('../../../assets/뱃지5.png')}
-                            resizeMode="contain"
-                            style={styles.badgeImage}
-                        />
-                        <Text style={styles.badgeText}>출석상</Text>
-                    </View>
-                    <View style={styles.badgeItem}>
-                        <Image
-                            source={require('../../../assets/뱃지6.png')}
-                            resizeMode="contain"
-                            style={styles.badgeImage}
-                        />
-                        <Text style={styles.badgeText}>소통상</Text>
-                    </View>
-                    <View style={styles.badgeItem}>
-                        <Image
-                            source={require('../../../assets/뱃지7.png')}
-                            resizeMode="contain"
-                            style={styles.badgeImage}
-                        />
-                        <Text style={styles.badgeText}>최고상</Text>
-                    </View>
-                </ScrollView>
                 {/* 원형 프로그레스 표시 */}
                 <View style={styles.progressCircleContainer}>
                     {[...Array(Math.ceil(progressCircleConfigs.length / 3))].map(
@@ -631,50 +567,6 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#fff',
-    },
-    badgeTitle: {
-        height: 50,
-        width: '50%',
-        marginVertical: 10,
-        borderWidth: 1,
-        justifyContent: 'center',
-        alignSelf: 'center',
-        alignItems: 'center',
-        borderRadius: 10,
-        flexDirection: 'row',
-        borderColor: '#F29F05',
-        backgroundColor: 'white',
-        elevation: 5,
-    },
-    badgeTitleText: {
-        fontSize: 20,
-        color: '#333',
-        fontWeight: '600',
-    },
-    badgeIcon: {
-        color: '#F29F05',
-        marginLeft: 5,
-        
-    },
-    badgeContainer: {
-        flexDirection: 'row',
-        paddingHorizontal: 10,
-        paddingVertical: 5,
-        backgroundColor: 'white',
-        height: 100,
-    },
-    badgeItem: {
-        alignItems: 'center',
-        justifyContent: 'center',
-        marginHorizontal: 5,
-    },
-    badgeImage: {
-        width: 50,
-        height: 50,
-        opacity: 0.3
-    },
-    badgeText: {
-        color: 'black',
     },
     // 프로그레스 서클 영역 스타일
     progressCircleContainer: {
