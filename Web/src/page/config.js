13d0b355df945a57027c9da668f1779b591bf80a
epuchi <jite1214@gmail.com>
2024-10-25 14:09:11 +0900

    서버 아이피 수정

--- Web/src/page/config.js
+++ Web/src/page/config.js
@@ -1,6 +1,6 @@
 const config = {
-    serverUrl: 'http://172.16.106.43:4000',
-    photoUrl : 'http://172.16.106.43:4000'
+    serverUrl: 'http://14.6.152.230:4000',
+    photoUrl : 'http://14.6.152.230:4000'
 };
 
 export default config;
