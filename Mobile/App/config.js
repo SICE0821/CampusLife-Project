c426fa4a5d3fdff4ee4d3898179c41f8b83965a7
epuchi <jite1214@gmail.com>
2024-10-30 17:48:29 +0900

    push

--- Mobile/App/config.js
+++ Mobile/App/config.js
@@ -1,6 +1,6 @@
 const config = {
-    serverUrl: 'http://14.6.152.230:3000',
-    photoUrl : 'http://14.6.152.230:3000'
+    serverUrl: 'http://172.16.106.231:3000',
+    photoUrl : 'http://172.16.106.231:3000'
 };
 
 export default config;
