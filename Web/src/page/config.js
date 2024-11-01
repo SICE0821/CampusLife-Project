abed7ff3cfeb7e2a106558eb7db01535377f011f
epuchi <jite1214@gmail.com>
2024-11-01 10:14:44 +0900

    rne

--- Web/src/page/config.js
+++ Web/src/page/config.js
@@ -1,11 +1,6 @@
 const config = {
-<<<<<<< HEAD
     serverUrl: 'http://172.16.106.231:4000',
     photoUrl : 'http://172.16.106.231:4000'
-=======
-    serverUrl: 'http://172.16.108.108:4000',
-    photoUrl : 'http://172.16.108.108:4000'
->>>>>>> Feat/AttendancePage
 };
 
 export default config;
